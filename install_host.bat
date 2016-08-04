5c3c5032674aafaddb05bbdecce8765a72d66a62
alec <alecweber1994@gmail.com>
2016-08-04 00:48:18 -0400

    commits for future windows support
    added windows install script

--- install_host.bat
+++ install_host.bat
@@ -1,1 +1,10 @@
-TODO
+:: taken from
+:: https://chromium.googlesource.com/chromium/src/+/master/chrome/common/extensions/docs/examples/api/nativeMessaging/host/install_host.bat
+
+:: Change HKCU to HKLM if you want to install globally.
+
+
+
+
+REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.awlnx.video_connector.json" /ve /t REG_SZ /d "%~dp0com.awlnx.video_connector-win.json" /f
+
