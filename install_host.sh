049b620a301430e2b1d93fd0319b7a74769c8035
alec <awlnx@debian-awlnx-laptop>
2016-08-18 12:11:05 -0400

    fix #8

--- install_host.sh
+++ install_host.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-HostName=com.awlnx.video_connector
-Platform=$(uname -s)
+HostName=com.awlnx.video_connector Platform=$(uname -s)
 #NativeMessagingDirChrome
 #NativeMessagingDirChromium
-Ichrome=false
-Ichromium=false
+Ichrome="false"
+Ichromium="false"
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 if [ "$Platform" == "Darwin" ]; then
 	if [ "$(whoami)" == "root" ]; then
@@ -40,34 +39,60 @@ fi
 echo "Do you wish to install for chrome?"
 select yn in "Yes" "No"; do
 	    case $yn in
-		            Yes ) Ichrome=true; break;;
-			            No ) break;;
+		       Yes) 
+			       Ichrome="true"
+			       echo "are you using the developers version of chrome?"
+			       select yn in "Yes" "No"; do
+				       case $yn in 
+					       Yes) 
+						NativeMessagingDirChrome="$HOME/.config/google-chrome-unstable/NativeMessagingHosts"
+						   break 
+						   ;;
+					       No)
+						       break 
+						       ;;
+				       esac
+			       done
+
+			       break
+				    ;;
+	               No) 
+			       break
+			            ;;
 				        esac
 				done
 echo "Do you wish to install for chromium?"
 select yn in "Yes" "No"; do
 	    case $yn in
-		            Yes ) Ichromium=true; break;;
-			            No ) break;;
+		            Yes) 
+				    Ichromium="true"
+				    break
+				    ;;
+			            No) 
+					    break
+					    ;;
 				        esac
 				done
 
 
-
-if [ "$Ichrome" ]; then
+if  [ $Ichrome = "true" ] ; then
 	mkdir -p "$NativeMessagingDirChrome"
+	echo "$DIR/$HostName.json"
+	echo "$NativeMessagingDirChrome/$HostName.json"
 	cp "$DIR/$HostName.json" "$NativeMessagingDirChrome/."
-	sed -i "s:#\*#:$DIR/$HostName.json:" "$NativeMessagingDirChrome/$HostName.json"
+	sed -i "s:#\*#:$DIR/connector:" "$NativeMessagingDirChrome/$HostName.json"
 	sed -i "s:\\\:\\\\\\\:g" "$NativeMessagingDirChrome/$HostName.json"
+	echo "installed for chrome"
 fi
 
-if [ "$Ichromium" ]; then
+if  [ $Ichromium = "true" ]; then
 	mkdir -p "$NativeMessagingDirChromium"
 	echo "$DIR/$HostName.json"
 	echo "$NativeMessagingDirChromium/$HostName.json"
 	echo $(cp "$DIR/$HostName.json" "$NativeMessagingDirChromium/$HostName.json")
 	sed -i "s:#\*#:$DIR/connector:" "$NativeMessagingDirChromium/$HostName.json"
 	sed -i "s:\\\:\\\\\\\:g" "$NativeMessagingDirChromium/$HostName.json"
+	echo "installed for chromium"
 fi
 
 
