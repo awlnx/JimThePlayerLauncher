d4ab32ae2c1109e4b0caafc0a78b265f6d881b2c
alec <alecweber1994@gmail.com>
2016-09-17 12:14:18 -0400

    refactor
    * convertion of all variables and function names to camelCase
    * moved all code into MediaGrabber object as to not dirty global space

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-// Media Grabber Class //
+// Media Grabber namespace//
 // code execution is on line 115 
 
 /******************************
@@ -34,123 +34,25 @@ Array.prototype.binIndexOf = function (comparison) {
 
 ****************************************/
 function MediaGrabber() {
-    this.linkshash = {};
+    this.linksHash = {};
     this.type = [];
-    this.activetabid = undefined;
-    this.returndata = function() {
-        return this.linkhash[this.activetabid];
-    };
+    this.activeTabId = undefined;
+    /*this.returndata = function() {
+        return this.linkhash[this.activeTabId];
+    };*/
     this.listener = function(details) {
         if (this.filter(details.responseHeaders)) {
-            if (this.linkshash[this.activetabid].indexOf(details.url) < 0)
-                this.linkshash[this.activetabid].push(details.url);
+            if (this.linksHash[this.activeTabId].indexOf(details.url) < 0){
+                this.linksHash[this.activeTabId].push(details.url);
+		this.evalIconState();
+	    }
+
         }
     };
-}
-
-MediaGrabber.prototype.setfilter = function(arr) {
-    this.type = arr;
-    this.type.sort();
-};
-MediaGrabber.prototype.filter = function(headers) {
-    var contenttype = headers.find(function(dict) {
-        return (dict.name === 'Content-Type' || dict.name === 'content-type'); //sometimes returns undefined;
-    }.bind(this));
-    if (contenttype === undefined) {
-	    return false;
-    }
-    if (this.type.binIndexOf(contenttype.value) != -1){
-	    return true;
-    }
-    return false;
-    };
-MediaGrabber.prototype.evaliconstate = function() {
-    if (this.linkshash[this.activetabid].length !== 0) {
-        chrome.browserAction.setIcon({
-            path: 'icons/play.png'
-        });
-    } else {
-        chrome.browserAction.setIcon({
-            path: 'icons/notplay.png'
-        });
-    }
-
-};
-MediaGrabber.prototype.startlistener = function() {
-    chrome.webRequest.onHeadersReceived.addListener(this.listener.bind(this), {
-        urls: ["<all_urls>"]
-    }, ["responseHeaders"]);
-};
-MediaGrabber.prototype.startgrabber = function(items) {
-	/* Init */
-    chrome.tabs.query({
-        active: true,
-        lastFocusedWindow: true
-    }, function(tabs) {
-        var tabid = tabs[0].id;
-        this.activetabid = tabid;
-        this.linkshash[tabid] = [];
-        this.startlistener();
-    }.bind(this));
-        /*OnActivted*/ 
-    chrome.tabs.onActivated.addListener(function(info) {
-        chrome.webRequest.onHeadersReceived.removeListener(this.listener);
-        if (this.linkshash[info.tabId] === undefined) {
-            this.linkshash[info.tabId] = [];
-        }
-        this.activetabid = info.tabId;
-        this.startlistener();
-        this.evaliconstate();
-	    /*OnUpdated*/
-        chrome.tabs.onUpdated.addListener(function(tabid, info) {
-            if (info.url !== undefined)
-                this.linkshash[this.activetabid] = [];
-            this.evaliconstate();
-        }.bind(this));
-    }.bind(this));
-        /*OnClicked*/ 
-    chrome.browserAction.onClicked.addListener(function() {
-        chrome.tabs.get(this.activetabid, function(tab) {
-		var pass = false;
-		var domains = items.Domains;
-		// O(N) speed
-		for (var i = 0; i < domains.length ; i++){
-                      var match= new RegExp('.*' + domains[i] + '.*');
-			if (match.test(tab.url)) { 
-				//domains[i].match( )) ) {
-
-					pass=true;
-					break;
-			}
-		}
 
 
-            //if (/.*youtube.*/.test(tab.url) ||/.*twitch.*/.test(tab.url) || /.*dailymotion.*/.test(tab.url)) {
-	     if (pass) {
-                Link = tab.url;
-
-            } else {
-                Link = (this.linkshash[this.activetabid])[0];
-            }
-            if (Link === undefined) {
-                console.log("no link");
-                return;
-            }
-            chrome.runtime.sendNativeMessage("com.awlnx.video_connector", {
-                player: items.Player,
-                link: Link
-            });
-        }.bind(this));
-    }.bind(this));
-};
-MediaGrabber.prototype.links = function() {
-    return this.linkshash[this.activetabid];
-};
-
-//**start**//
 var player = {};
-MediaEngine = new MediaGrabber();
-MediaEngine.setfilter(['video/mp4', 'video/x-flv', 'video/webm']);
+this.setFilter(['video/mp4', 'video/x-flv', 'video/webm']);
 
 chrome.storage.local.get({
 	Player: null,
@@ -161,10 +63,10 @@ chrome.storage.local.get({
 		}
 		else {
 			player = items;
-                    MediaEngine.startgrabber(player);
+                    this.startGrabber(player);
 		}
 
-	});
+	}.bind(this));
 
 chrome.storage.onChanged.addListener(function(changes,areaname) {
 	chrome.storage.local.get({
@@ -176,3 +78,124 @@ chrome.storage.onChanged.addListener(function(changes,areaname) {
 	});
 });
 
+
+
+
+
+
+
+
+
+
+}
+
+
+
+MediaGrabber.prototype.setFilter = function(arr) {
+    this.type = arr;
+    this.type.sort();
+};
+MediaGrabber.prototype.filter = function(headers) {
+    var contentType = headers.find(function(dict) {
+        return (dict.name === 'Content-Type' || dict.name === 'content-type'); //sometimes returns undefined;
+    }.bind(this));
+    if (contentType === undefined) {
+	    return false;
+    }
+    if (this.type.binIndexOf(contentType.value) != -1){
+	    return true;
+    }
+    return false;
+    };
+MediaGrabber.prototype.evalIconState = function() {
+    if (this.linksHash[this.activeTabId].length !== 0) {
+        chrome.browserAction.setIcon({
+            path: 'icons/play.png'
+        });
+    } else {
+        chrome.browserAction.setIcon({
+            path: 'icons/notplay.png'
+        });
+    }
+
+};
+MediaGrabber.prototype.startListener = function() {
+    chrome.webRequest.onHeadersReceived.addListener(this.listener.bind(this), {
+        urls: ["<all_urls>"]
+    }, ["responseHeaders"]);
+};
+MediaGrabber.prototype.startGrabber = function(items) {
+
+	/* Init */
+	//get first tab, clear data and start listening to GET requests
+    chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true
+    }, function(tabs) {
+        var tabId = tabs[0].id;
+        this.activeTabId = tabId;
+        this.linksHash[tabId] = [];
+        this.startListener();
+    }.bind(this));
+
+
+        /*OnActivted*/ 
+     chrome.tabs.onActivated.addListener(function(info) {
+        //chrome.webRequest.onHeadersReceived.removeListener(this.listener);
+        if (this.linksHash[info.tabId] === undefined) {
+            this.linksHash[info.tabId] = [];
+        }
+        this.activeTabId = info.tabId;
+        this.startListener();
+        this.evalIconState();
+     }.bind(this));
+	    
+     
+     
+        /*OnUpdated*/
+        chrome.tabs.onUpdated.addListener(function(tabid, info) {
+            if (info.url !== undefined)
+                this.linksHash[this.activeTabId] = [];
+        }.bind(this));
+
+        
+	/*OnClicked*/ 
+    chrome.browserAction.onClicked.addListener(function() {
+
+        chrome.tabs.get(this.activeTabId, function(tab) {
+		var pass = false;
+		var domains = items.Domains; // passed into fuction from storage data
+		var match;
+                var l = domains.length; // speed hack for loop 
+		for (var i = 0; i < l ; i++){
+                        match = new RegExp('.*' + domains[i] + '.*');
+			if (match.test(tab.url)) { 
+					pass=true;
+					break;
+			}
+		}
+	     if (pass) {
+                Link = tab.url;
+
+            } else {
+                Link = (this.linksHash[this.activeTabId])[0];
+            }
+            
+	     chrome.runtime.sendNativeMessage("com.awlnx.video_connector", {
+                player: items.Player,
+                link: Link
+            });
+
+        }.bind(this));
+
+    }.bind(this));
+
+
+};
+MediaGrabber.prototype.links = function() {
+    return this.linksHash[this.activeTabId];
+};
+
+//**start**//
+//
+MediaEngine = new MediaGrabber();
